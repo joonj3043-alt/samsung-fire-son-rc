4f1745d29a8ec7fe2deba7e97c3146ba00388801
joonj3043-alt <joonj3043@gmail.com>
2026-09-21 22:28:43 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+window.SITE_CONFIG = {
+  phone: "", // TODO: 숫자만 입력 예: 01012345678
+  phoneDisplay: "", // TODO: 표시 형식 예: 010-1234-5678
+  email: "", // TODO: 상담용 이메일
+  kakaoUrl: "", // 선택: 카카오톡 채널 URL
+  officialUrl: "https://www.samsungfire.com/",
+  siteUrl: "", // TODO: 최종 도메인 예: https://example.com (끝 슬래시 제외)
+  formEndpoint: "" // TODO: Formspree 엔드포인트 예: https://formspree.io/f/xxxxxxx
+};
