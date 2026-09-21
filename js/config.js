c5682ebb7a929ecdc9575920c166bccf4f61de13
joonj3043-alt <joonj3043@gmail.com>
2026-09-21 23:24:09 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.SITE_CONFIG = {
-  phone: "", // TODO: 숫자만 입력 예: 01012345678
-  phoneDisplay: "", // TODO: 표시 형식 예: 010-1234-5678
-  email: "", // TODO: 상담용 이메일
+  phone: "01038173043",
+  phoneDisplay: "010-3817-3043",
+  email: "ssjmvp123@naver.com",
   kakaoUrl: "", // 선택: 카카오톡 채널 URL
   officialUrl: "https://www.samsungfire.com/",
   siteUrl: "", // TODO: 최종 도메인 예: https://example.com (끝 슬래시 제외)
